5c61da683ea1d16b04b46a30db77a9702026c96d
Nadav Mermer <nadav@mermers.net>
2020-08-17 14:13:50 +0300

    Updated calculator's constants and add monthly netto sum

--- calculator.py
+++ calculator.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-INVESTED_DEFAULT = 100000
 TAX = 15
-INTEREST_PCT_OVER_2YR = 0.5
-INTEREST_PCT_OVER_1YR = 0.4
-INTEREST_PCT_FLUID = 1/3
 SUCCESS_COMMISSION_PCT = 10
 INVEST_YEARLY_COMMISSION_PCT = 0.5
+INTEREST_PCT_OVER_2YR = 6.5/12
+INTEREST_PCT_OVER_1YR = 5.5/12
+INTEREST_PCT_FLUID = 5/12
 
 def calc_success_commission_pct(invested, interest_pct):
     return invested * interest_pct / 100 * SUCCESS_COMMISSION_PCT / 100
@@ -31,6 +30,12 @@ def calc_tara(invested, interest_pct):
 def calc_neto(invested, interest_pct):
     return calc_monthly_interest(invested, interest_pct) - calc_tara(invested, interest_pct)
 
+def sum_netos(invested_2yr, invested_1yr, invested_fluid):
+    monthly_interest_2yr = calc_neto(invested_2yr, INTEREST_PCT_OVER_2YR)
+    monthly_interest_1yr = calc_neto(invested_1yr, INTEREST_PCT_OVER_1YR)
+    monthly_interest_fluid = calc_neto(invested_fluid, INTEREST_PCT_FLUID)
+
+    return monthly_interest_2yr + monthly_interest_1yr + monthly_interest_fluid
 
 if __name__ == "__main__":
     import sys
